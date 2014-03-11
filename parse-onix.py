6baf203fdb266fa900a2865aec2b5aee6c6e2ab5
michel.blaauw@gmail.com <michel.blaauw@gmail.com>
2014-03-11 21:22:00 +0100

    initial commit

--- parse-onix.py
+++ parse-onix.py
@@ -22,99 +22,146 @@ extentType = list()
 extentUnit = list()
 extentValue = list()
 
-onxFile = 'data/TB30_totaal_2014-03_073van073.onx'
-soup = BeautifulSoup(open(onxFile, 'r'), features='lxml')
 
-# collect unique ID's
-productBlock = soup.find_all('product')
-for eachProductBlock in productBlock:
-    idValue.append(eachProductBlock.idvalue.string)
-    idType.append(eachProductBlock.productidtype.string)
+def parseOnxFiles(onxFile):
+    soup = BeautifulSoup(open(onxFile, 'r'), features='lxml')
 
-    # collect publishing details
-    publishingSoup = BeautifulSoup(str(eachProductBlock), features='lxml')
-    publishingBlock = publishingSoup.find_all('publishingdate')
-    for eachPublishBlock in publishingBlock:
-        publishingRole.append(eachPublishBlock.publishingdaterole.string)
-        publishingDateFormat.append(eachPublishBlock.dateformat.string)
-        publishingDate.append(eachPublishBlock.date.string)
+    # collect unique ID's
+    productBlock = soup.find_all('product')
+    for eachProductBlock in productBlock:
+        idValue.append(eachProductBlock.idvalue.string)
+        idType.append(eachProductBlock.productidtype.string)
 
-    # collect descriptive details
-    descriptiveSoup = BeautifulSoup(str(eachProductBlock), features='lxml')
-    descriptiveBlock = descriptiveSoup.find_all('descriptivedetail')
-    for eachDescriptiveBlock in descriptiveBlock:
-        productComposition.append(eachDescriptiveBlock.productcomposition.string)
-        productForm.append(eachDescriptiveBlock.productform.string)
+        # collect publishing details
+        publishingSoup = BeautifulSoup(str(eachProductBlock), features='lxml')
+        publishingBlock = publishingSoup.find_all('publishingdate')
+        for eachPublishBlock in publishingBlock:
+            publishingRole.append(eachPublishBlock.publishingdaterole.string)
+            publishingDateFormat.append(eachPublishBlock.dateformat.string)
+            publishingDate.append(eachPublishBlock.date.string)
 
-        chkEditionNumber = descriptiveSoup.find_all('editionnumber')
-        chkEditionStatement = descriptiveSoup.find_all('editionstatement')
-        if len(chkEditionNumber) != 0:
-            editionNumber.append(eachDescriptiveBlock.editionnumber.string)
-        else:
-            editionNumber.append('')
-        if len(chkEditionStatement) != 0:
-            editionStatement.append(eachDescriptiveBlock.editionstatement.string)
-        else:
-            editionStatement.append('')
+        # collect descriptive details
+        descriptiveSoup = BeautifulSoup(str(eachProductBlock), features='lxml')
+        descriptiveBlock = descriptiveSoup.find_all('descriptivedetail')
+        for eachDescriptiveBlock in descriptiveBlock:
+            productComposition.append(eachDescriptiveBlock.productcomposition.string)
+            productForm.append(eachDescriptiveBlock.productform.string)
 
-        illustratedType.append(eachDescriptiveBlock.illustrated.string)
-        languageRole.append(eachDescriptiveBlock.languagerole.string)
-        languageCode.append(eachDescriptiveBlock.languagecode.string)
-        subjectSchemeIdentifier.append(eachDescriptiveBlock.subjectschemeidentifier.string)
-        subjectCode.append(eachDescriptiveBlock.subjectcode.string)
+            chkEditionNumber = descriptiveSoup.find_all('editionnumber')
+            chkEditionStatement = descriptiveSoup.find_all('editionstatement')
+            chkIllustratedType = descriptiveSoup.find_all('illustrated')
+            chkLanguageRole = descriptiveSoup.find_all('languagerole')
+            chkLanguageCode = descriptiveSoup.find_all('languagecode')
 
-        # collect extent details
-        extentSoup = BeautifulSoup(str(eachDescriptiveBlock), features='lxml')
-        extentBlock = extentSoup.find_all('extent')
-        if len(extentBlock) != 0:
-            for eachExtentBlock in extentBlock:
+            if len(chkEditionNumber) != 0:
+                editionNumber.append(eachDescriptiveBlock.editionnumber.string)
+            else:
+                editionNumber.append('')
 
-                chkExtentType = eachExtentBlock.find_all('extenttype')
-                if len(chkExtentType) != 0 and eachExtentBlock.extenttype.string == '00':
-                    extentType.append(eachExtentBlock.extenttype.string)
-                else:
-                    extentType.append('')
+            if len(chkEditionStatement) != 0:
+                editionStatement.append(eachDescriptiveBlock.editionstatement.string)
+            else:
+                editionStatement.append('')
 
-                chkExtentValue = eachExtentBlock.find_all('extentvalue')
-                if len(chkExtentValue) != 0 and eachExtentBlock.extenttype.string == '00':
-                    extentValue.append(eachExtentBlock.extentvalue.string)
-                else:
-                    extentValue.append('')
+            if len(chkIllustratedType) != 0:
+                illustratedType.append(eachDescriptiveBlock.illustrated.string)
+            else:
+                illustratedType.append('')
 
-                chkExtentUnit = eachExtentBlock.find_all('extentunit')
-                if len(chkExtentUnit) != 0 and eachExtentBlock.extenttype.string == '00':
-                    extentUnit.append(eachExtentBlock.extentunit.string)
-                else:
-                    extentUnit.append('')
+            if len(chkIllustratedType) != 0:
+                languageRole.append(eachDescriptiveBlock.languagerole.string)
+            else:
+                languageRole.append('')
+
+            if len(chkIllustratedType) != 0:
+                languageCode.append(eachDescriptiveBlock.languagecode.string)
+            else:
+                languageCode.append('')
+
+            subjectSchemeIdentifier.append(eachDescriptiveBlock.subjectschemeidentifier.string)
+            subjectCode.append(eachDescriptiveBlock.subjectcode.string)
+
+            # collect extent details
+            extentSoup = BeautifulSoup(str(eachDescriptiveBlock), features='lxml')
+            extentBlock = extentSoup.find_all('extent')
+            if len(extentBlock) != 0:
+                for eachExtentBlock in extentBlock:
+
+                    chkExtentType = eachExtentBlock.find_all('extenttype')
+                    if len(chkExtentType) != 0 and eachExtentBlock.extenttype.string == '00':
+                        extentType.append(eachExtentBlock.extenttype.string)
+                    else:
+                        extentType.append('')
+
+                    chkExtentValue = eachExtentBlock.find_all('extentvalue')
+                    if len(chkExtentValue) != 0 and eachExtentBlock.extenttype.string == '00':
+                        extentValue.append(eachExtentBlock.extentvalue.string)
+                    else:
+                        extentValue.append('')
+
+                    chkExtentUnit = eachExtentBlock.find_all('extentunit')
+                    if len(chkExtentUnit) != 0 and eachExtentBlock.extenttype.string == '00':
+                        extentUnit.append(eachExtentBlock.extentunit.string)
+                    else:
+                        extentUnit.append('')
 
 
-result = np.array(zip(idValue, idType, publishingRole, publishingDateFormat,publishingDate,productComposition, productForm, editionStatement, editionNumber, illustratedType, languageRole, languageCode, subjectSchemeIdentifier, subjectCode, extentType, extentUnit, extentValue))
+    result = np.array(zip(idValue, idType, publishingRole, publishingDateFormat,publishingDate,productComposition, productForm, editionStatement, editionNumber, illustratedType, languageRole, languageCode, subjectSchemeIdentifier, subjectCode, extentType, extentUnit, extentValue))
 
-conn = sqlite3.connect('sample.db')
-cursor = conn.cursor()
-cursor.execute('''create table data (
-	idValue NVARCHAR(13),
-	idType NVARCHAR(5),
-	publishingRole NVARCHAR(5),
-	publishingDateFormat NVARCHAR(5),
-	publishingDate NVARCHAR(8),
-	productComposition NVARCHAR(5),
-	productForm NVARCHAR(5),
-	editionStatement NVARCHAR(15),
-	editionNumber NVARCHAR(5),
-	illustratedType NVARCHAR(5),
-	languageRole NVARCHAR(5),
-	languageCode NVARCHAR(5),
-	subjectSchemeIdentifier NVARCHAR(5),
-	subjectCode NVARCHAR(5),
-	extentType NVARCHAR(5),
-	extentUnit NVARCHAR(5),
-	extentValue  NVARCHAR(5)
-)''')
-conn.commit()
+    return result
 
-cursor.executemany('''insert into data values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', map(tuple, result.tolist()))
-conn.commit()
-cursor.close()
-conn.close()
+
+def store_results(npArray, dbName = 'sample.db'):
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+    cursor.executemany('''insert into data values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', map(tuple, npArray.tolist()))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def create_initial_db(dbName = 'sample.db'):
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+    cursor.execute('''create table data (
+        idValue NVARCHAR(13),
+        idType NVARCHAR(5),
+        publishingRole NVARCHAR(5),
+        publishingDateFormat NVARCHAR(5),
+        publishingDate NVARCHAR(8),
+        productComposition NVARCHAR(5),
+        productForm NVARCHAR(5),
+        editionStatement NVARCHAR(15),
+        editionNumber NVARCHAR(5),
+        illustratedType NVARCHAR(5),
+        languageRole NVARCHAR(5),
+        languageCode NVARCHAR(5),
+        subjectSchemeIdentifier NVARCHAR(5),
+        subjectCode NVARCHAR(5),
+        extentType NVARCHAR(5),
+        extentUnit NVARCHAR(5),
+        extentValue  NVARCHAR(5)
+    )''')
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
+
+for i in range(40,74):
+    if len(str(i)) == 1:
+        fileNr = '00' + str(i)
+    else:
+        fileNr = '0' + str(i)
+
+
+    onxFile = 'titelbank/TB30_totaal_2014-03_' + fileNr + 'van073.onx/TB30_totaal_2014-03_' + fileNr + 'van073.onx'
+
+    result = parseOnxFiles(onxFile)
+    store_results(result)
+
+
+
 
